79a6c2dacb1324b3f465f1519a647de1390d7c8b
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-10-21 11:00:10 -0300

    Creadas la entidad User y el ENUM para los roles

---
+++ src/main/java/com/solarix_api/ecommerce_api/model/Role.java
@@ -0,0 +1,9 @@
+package com.solarix_api.ecommerce_api.model;
+
+/**
+ * Roles de usuario para autenticación y autorización
+ */
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/solarix_api/ecommerce_api/model/User.java
@@ -0,0 +1,38 @@
+package com.solarix_api.ecommerce_api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Email debe ser válido")
+    @Column(unique = true, nullable = false)
+    @NotNull(message = "Email")
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    public User(String email, String password, Role role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+}
