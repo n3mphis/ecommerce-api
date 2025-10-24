44c7bb266decacd60de3d5e91e4188a96819bf84
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-10-24 11:52:03 -0300

    Creado UserController

---
+++ src/main/java/com/solarix_api/ecommerce_api/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.solarix_api.ecommerce_api.controller;
+
+import com.solarix_api.ecommerce_api.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<Authentication> me(Authentication authentication) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return ResponseEntity.ok(auth);
+    }
+}
