6abc61c7bdb0d3dae72aff243bf47aba13c52cea
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-10-24 10:22:55 -0300

    Creadas excepciones

---
+++ src/main/java/com/solarix_api/ecommerce_api/exception/AutenticacionFallidaException.java
@@ -0,0 +1,7 @@
+package com.solarix_api.ecommerce_api.exception;
+
+public class AutenticacionFallidaException extends RuntimeException {
+    public AutenticacionFallidaException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/solarix_api/ecommerce_api/exception/EmailNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.solarix_api.ecommerce_api.exception;
+
+public class EmailNoEncontradoException extends RuntimeException {
+    public EmailNoEncontradoException(String message) {
+        super(message);
+    }
+}
