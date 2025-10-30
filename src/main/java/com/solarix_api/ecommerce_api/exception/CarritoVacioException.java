a37876646690664673b222f81f8770148f38c67c
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-10-30 09:35:19 -0300

    Creada nueva excepcion si el carrito esta vacio

---
+++ src/main/java/com/solarix_api/ecommerce_api/exception/CarritoVacioException.java
@@ -0,0 +1,7 @@
+package com.solarix_api.ecommerce_api.exception;
+
+public class CarritoVacioException extends RuntimeException {
+    public CarritoVacioException(String message) {
+        super(message);
+    }
+}
