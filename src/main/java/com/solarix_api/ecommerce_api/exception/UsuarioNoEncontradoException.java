09b7795254b08340e233b0baa5a3c895478b0462
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-10-31 11:07:35 -0300

    Creada nueva excepcion

---
+++ src/main/java/com/solarix_api/ecommerce_api/exception/UsuarioNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.solarix_api.ecommerce_api.exception;
+
+public class UsuarioNoEncontradoException extends RuntimeException {
+    public UsuarioNoEncontradoException(String message) {
+        super(message);
+    }
+}
