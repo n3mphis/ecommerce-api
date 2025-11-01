65201d5bebb623ad3df04860e2693449fff46cd1
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-11-01 17:08:14 -0300

    Creada la configuracion para OpenAPI

---
+++ src/main/java/com/solarix_api/ecommerce_api/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package com.solarix_api.ecommerce_api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Solarix E-commerce API")
+                        .version("1.0")
+                        .description("API REST para e-commerce con JWT, carrito y checkout"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer")
+                .name("Authorization")
+                .in(SecurityScheme.In.HEADER);
+    }
+}
