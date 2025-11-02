f6b89802de4a7aafba2cb5f637c24acc4d6e4a8f
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-11-02 11:01:27 -0300

    Cambios en RegistrarRequest para validar email

--- src/main/java/com/solarix_api/ecommerce_api/dto/RegistrarRequest.java
+++ src/main/java/com/solarix_api/ecommerce_api/dto/RegistrarRequest.java
@@ -5,7 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 
 public record RegistrarRequest(
         @NotBlank(message = "El mail no puede estar vacío")
-        @Email(message = "El mail debe ser válido")
+        @Email(
+                message = "El mail debe ser válido",
+                regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")
         String email,
         @NotBlank(message = "La contraseña no puede estar vacía")
         String password
