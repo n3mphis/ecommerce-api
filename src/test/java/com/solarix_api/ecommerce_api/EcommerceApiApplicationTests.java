a4dc9d854a6664702001b79f2668e7d7cd9b3005
n3mphis <>
2025-11-02 17:46:58 -0300

    Cambios para skipear los tests

--- src/test/java/com/solarix_api/ecommerce_api/EcommerceApiApplicationTests.java
+++ src/test/java/com/solarix_api/ecommerce_api/EcommerceApiApplicationTests.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = {"spring.main.web-application-type=none"})
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.NONE,
+		properties = {"spring.main.web-application-type=none"})
 @ActiveProfiles("test")
 class EcommerceApiApplicationTests {
 
